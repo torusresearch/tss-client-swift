3cf68843010a40351e8c2b9d505363460ef6d2d5
ieow <cwlee0@gmail.com>
2023-07-14 12:39:34 +0800

    fix: update tests checking

--- Tests/tss-client-swiftTests/helperTests.swift
+++ Tests/tss-client-swiftTests/helperTests.swift
@@ -14,31 +14,26 @@ final class helpersTests: XCTestCase {
     
     func testGetLangrange () throws {
         let result = getLagrangeCoeffs([BigInt(50), BigInt(100)], BigInt(10))
-        print (result.serialize().hexString)
-        let expected = Data(hexString: "f1c71c71c71c71c71c71c71c71c71c7093de09848919ecaa352a3cda52dde84d")
-        XCTAssertEqual(result.serialize().suffix(32), expected)
+        let expected = "f1c71c71c71c71c71c71c71c71c71c7093de09848919ecaa352a3cda52dde84d".addLeading0sForLength64()
+        XCTAssertEqual(result.serialize().suffix(32).toHexString(), expected)
     }
     
     func testGetAdditiveCoeff () throws {
         let result = getAdditiveCoeff(isUser: true, participatingServerIndexes: [BigInt(100), BigInt(200), BigInt(300)], userTSSIndex: BigInt(10))
-        print(result.serialize().hexString)
-        let expected = Data(hexString: "71c71c71c71c71c71c71c71c71c71c7136869b1131759c8c55410d93eac2c7ab")
-        XCTAssertEqual(result.serialize().suffix(32), expected)
+        let expected = "71c71c71c71c71c71c71c71c71c71c7136869b1131759c8c55410d93eac2c7ab".addLeading0sForLength64()
+        XCTAssertEqual(result.serialize().suffix(32).toHexString(), expected)
     }
-    
     
     func testGetDenormaliseCoeff () throws {
         let result = getDenormaliseCoeff(party: BigInt(100), parties: [BigInt(100), BigInt(200)])
-        print (result)
-        let expected = Data(hexString: "7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a1")
-        XCTAssertEqual(result.serialize().suffix(32), expected)
+        let expected = "7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a1".addLeading0sForLength64()
+        XCTAssertEqual(result.serialize().suffix(32).toHexString(), expected)
     }
     
     func testGetDKLSCoeff () throws {
         let result = getDKLSCoeff(isUser: true, participatingServerIndexes: [BigInt(100), BigInt(200)], userTSSIndex: BigInt(100))
-        let expected = Data(hexString: "a57eb50295fad40a57eb50295fad40a4ac66b301bc4dfafaaa8d2b05b28fae1")
-        print(expected!.toHexString(), result.serialize().hexString, result.serialize().toHexString())
-        XCTAssertEqual(result.serialize().suffix(32), expected)
+        let expected = "a57eb50295fad40a57eb50295fad40a4ac66b301bc4dfafaaa8d2b05b28fae1".addLeading0sForLength64()
+        XCTAssertEqual(result.serialize().suffix(32).toHexString(), expected)
     }
     
     
