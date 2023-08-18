4adb1f11ebac381c1fecc40a5e038c1b0bfc734f
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-08-18 16:27:51 +0800

    fix: session comparison in socket events

--- Sources/tss-client-swift/TSSSocket.swift
+++ Sources/tss-client-swift/TSSSocket.swift
@@ -53,13 +53,12 @@ internal final class TSSSocket {
                     print("disconnected, party:" + String(party))
                 })
                 socket.on("precompute_complete", callback: { data, ack in
-                    if session != self.session {
-                        print("ignoring message for a different session...")
-                        return
-                    }
-                    
                     if let json = try? JSONSerialization.data(withJSONObject: data[0]) {
                         if let msg = try? JSONDecoder().decode(TssPrecomputeUpdate.self, from: json) {
+                            if msg.session != self.session {
+                                print("ignoring message for a different session...")
+                                return
+                            }
                             EventQueue.shared.addEvent(event: Event(message: String(msg.party), session: msg.session, party: Int32(msg.party), occurred: Date(), type: EventType.PrecomputeComplete))
                         } else {
                             EventQueue.shared.addEvent(event: Event(message: "Received json was not decodable", session: self.session, party: Int32(-1), occurred: Date(), type: EventType.SocketDataError))
@@ -72,14 +71,13 @@ internal final class TSSSocket {
                     }
                 })
                 socket.on("precompute_failed", callback: { data, ack in
-                    if session != self.session {
-                        print("ignoring message for a different session...")
-                        return
-                    }
-                    
                     if let json = try? JSONSerialization.data(withJSONObject: data[0])
                     {
                         if let msg = try? JSONDecoder().decode(TssPrecomputeUpdate.self, from: json) {
+                            if msg.session != self.session {
+                                print("ignoring message for a different session...")
+                                return
+                            }
                             EventQueue.shared.addEvent(event: Event(message: String(msg.party), session: msg.session, party: Int32(msg.party), occurred: Date(), type: EventType.PrecomputeError))
                         } else {
                             EventQueue.shared.addEvent(event: Event(message: "Received json was not decodable", session: self.session, party: Int32(-1), occurred: Date(), type: EventType.SocketDataError))
@@ -92,13 +90,12 @@ internal final class TSSSocket {
                     }
                 })
                 socket.on("send", callback: { data, ack in
-                    if session != self.session {
-                        print("ignoring message for a different session...")
-                        return
-                    }
-                    
                     if let json = try? JSONSerialization.data(withJSONObject: data[0]) {
                         if let msg = try? JSONDecoder().decode(TssRecvMsg.self, from: json) {
+                            if msg.session != self.session {
+                                print("ignoring message for a different session...")
+                                return
+                            }
                             MessageQueue.shared.addMessage(msg: Message(session: msg.session, sender: UInt64(Int64(msg.sender)), recipient: UInt64(Int64(msg.recipient)), msgType: msg.msg_type, msgData: msg.msg_data))
                             let tag = msg.msg_type.split(separator: "~")[1]
                             print("dkls: Received message \(tag), sender: `\(msg.sender)`, receiver: `\(msg.recipient)`")
