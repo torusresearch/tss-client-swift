7e43662ed0c111b51142766b2db239e6a7b1d610
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-08-22 22:02:15 +0800

    fix: lock starscream for ios until fix is released for  socket.io

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git",from: "1.7.2"),
         .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMajor(from: "0.6.0")),
-        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "16.0.1"))
+        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "16.0.1")),
+        .package(url: "https://github.com/daltoniam/Starscream", .exactItem("4.0.4")),
     ],
     targets: [
         .binaryTarget(name: "libdklsnative",
@@ -25,7 +26,7 @@ let package = Package(
         ),
         .target(
             name: "tss-client-swift",
-            dependencies: ["BigInt", "CryptoSwift", "secp256k1", "SocketIO", "libdkls"]),
+            dependencies: ["BigInt", "CryptoSwift", "secp256k1", "Starscream", "SocketIO", "libdkls"]),
         .testTarget(
             name: "tss-client-swiftTests",
             dependencies: ["tss-client-swift", "BigInt"]),
