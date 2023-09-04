9eb415f7e3fcd4d711678f8c0bf641de3c06b9a3
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-09-04 12:12:44 +0200

    fix: unlock starscream

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
         .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git",from: "1.7.2"),
         .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMajor(from: "0.12.2")),
         .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "16.0.1")),
-        .package(url: "https://github.com/daltoniam/Starscream", .exactItem("4.0.4")),
     ],
     targets: [
         .binaryTarget(name: "libdkls",
@@ -26,7 +25,7 @@ let package = Package(
         ),
         .target(
             name: "tss-client-swift",
-            dependencies: ["BigInt", "CryptoSwift", "secp256k1", "Starscream", "SocketIO", "dkls"]),
+            dependencies: ["BigInt", "CryptoSwift", "secp256k1", "SocketIO", "dkls"]),
         .testTarget(
             name: "tss-client-swiftTests",
             dependencies: ["tss-client-swift", "BigInt"]),
